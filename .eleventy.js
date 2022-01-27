141c4172579b7b042d061ce3324bdf47db7453e8
Jacob Erholt <56915193+JHErholt@users.noreply.github.com>
2022-01-27 10:38:41 +0100

    Update .eleventy.js
    Updated the .eleventy to support sass and js and eventually json

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,25 @@
-const { DateTime } = require("luxon");
+const { DateTime } = require('luxon');
 
 module.exports = function(eleventyConfig) {
-   
-    eleventyConfig.addPassthroughCopy("./src/style.css");
-    eleventyConfig.addPassthroughCopy("./src/assets");
-    eleventyConfig.addPassthroughCopy("./src/admin");
 
-    eleventyConfig.addFilter("postDate", (dateObj) => {
+    eleventyConfig.addWatchTarget('./src/sass/')
+
+    eleventyConfig.addPassthroughCopy('./src/css');
+    eleventyConfig.addPassthroughCopy('./src/js');
+    eleventyConfig.addPassthroughCopy('./src/json');
+    eleventyConfig.addPassthroughCopy('./src/assets');
+
+    eleventyConfig.addFilter('postDate', (dateObj) => {
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
     })
+    eleventyConfig.addFilter('slugify', function(value) {
+        return value;
+    });
 
-    return {
+    return{
         dir: {
-            input: "src",
-            output: "public"
+            input: 'src',
+            output: 'public'
         }
     };
 }
